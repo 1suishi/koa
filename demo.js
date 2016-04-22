6cb931499bed21801d3e3e29e9ce996b91fec05c
benniuer <1962323664@qq.com>
2016-04-22 18:02:34 +0800

    update

--- demo.js
+++ demo.js
@@ -1,9 +1,19 @@
 var koa = require("koa");
-var app = koa();
+var router = require('koa-router');
 
-app.use(function *(){
-	var path = this.path;
-	this.body = path;
+var app = koa();
+app.use(router(app));
+
+
+app.get('/', function *(next) {
+	this.body = "这是root"
 });
+app.get('/detail/:id', function *(next) {
+	//我是详情页面
+	//:id 是路由通配规则，示例请求 /detail/123 就会进入该 generator function 逻辑
+	var id = this.params.id; //123
+	this.body = "这是详情页" +id;
+});
+
 
 app.listen(3000);
